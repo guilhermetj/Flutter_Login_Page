5947ab2d092daf8cba3b7dc80fd794831168d966
Guilherme pereira <52841800+guilhermetj@users.noreply.github.com>
2023-05-22 14:31:48 -0300

    Criando page com drawer

--- lib/Pages/main_page.dart
+++ lib/Pages/main_page.dart
@@ -10,8 +10,55 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Meu app")),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Main Page")),
+        drawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Dados Cadastrais"),
+                    ),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Termos de uso e privacidade"),
+                    ),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Dados Cadastrais"),
+                    ),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
